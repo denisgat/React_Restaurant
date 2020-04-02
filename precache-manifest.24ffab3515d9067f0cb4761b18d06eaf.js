a31ed27b10181ac7d7fb85cbcdcf57083ce092f6
denisgat <denisgatotho@gmail.com>
2020-04-02 13:12:07 -0400

    Updates

---
+++ precache-manifest.24ffab3515d9067f0cb4761b18d06eaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63fa55966f576cdcfb9b2a3982b7f15e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cbf41a6a8857b2df392",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "9d19b7ccc95e6a572827",
+    "url": "/static/css/main.c782fd48.chunk.css"
+  },
+  {
+    "revision": "1cbf41a6a8857b2df392",
+    "url": "/static/js/2.02b185eb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.02b185eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d19b7ccc95e6a572827",
+    "url": "/static/js/main.3cfbe697.chunk.js"
+  },
+  {
+    "revision": "5209f98888dacbdc9a6e",
+    "url": "/static/js/runtime-main.a9281086.js"
+  }
+]);
